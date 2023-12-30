f5b8b4b420ffdffd5e0d357cbfe4980f675837d4
lusaisai <lusaisai@163.com>
2023-12-30 09:25:11 +0800

    Create v2ray.sh

---
+++ v2ray.sh
@@ -0,0 +1,28 @@
+# install
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+
+
+# change config
+cat << EOF > /usr/local/etc/v2ray/config.json
+{
+  "inbounds": [{
+    "port": 10086,
+    "protocol": "vmess",
+    "settings": {
+      "clients": [{ "id": "$(uuidgen)" }]
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom",
+    "settings": {}
+  }]
+}
+EOF
+
+# service
+service v2ray restart
+service v2ray status
+
+# check config
+ip addr
+cat /usr/local/etc/v2ray/config.json
