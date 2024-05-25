3e11f89af97353f7b5323b23cf8adf9ce6da67ad
lusaisai <lusaisai@163.com>
2024-05-25 21:38:54 +0800

    Update hysteria2.sh

--- hysteria2.sh
+++ hysteria2.sh
@@ -50,6 +50,52 @@ cat /etc/hysteria/config.yaml
 # fingerprint for local
 openssl x509 -noout -fingerprint -sha256 -in /certs/3.crt
 
+# or pem
+cat /certs/3.key /certs/3.crt > /certs/3.pem
+cat /certs/3.pem
+
+# client for sing-box
+{
+  "inbounds": [
+    {
+      "type": "mixed",
+      "tag": "mixed-in",
+      "listen": "::",
+      "listen_port": 1080
+    }
+  ],
+  "outbounds": [
+    {
+      "type": "hysteria2",
+      "server": "",
+      "server_port": 443,
+      "up_mbps": 50,
+      "down_mbps": 500,
+      "password": "",
+      "tls": {
+        "enabled": true,
+        "server_name": "linux.com",
+        "certificate_path": "/etc/3.pem"
+      }
+    }
+  ]
+}
+
+# openwrt init.d
+root@OW:/etc/init.d# cat sing-box
+#!/bin/sh /etc/rc.common
+# Example script
+# Copyright (C) 2007 OpenWrt.org
+
+START=99
+
+USE_PROCD=1
+
+start_service() {
+        procd_open_instance
+        procd_set_param command sing-box -c /etc/sing-box/config.json run
+        procd_close_instance
+}
 
 # client config.yaml
 server: :443
